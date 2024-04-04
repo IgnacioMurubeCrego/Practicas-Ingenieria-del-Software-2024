c7e254ec2cc332b980ea708d82c1b43b14644549
IgnacioMurubeCrego <imurubec@alumnos.nebrija.es>
2024-04-04 18:34:25 +0200

    1st release

---
+++ demo/src/main/java/com/example/demo/EmergencyNumScreenController.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static com.example.demo.DocDocGoApplication.currentStage;
+
+public class EmergencyNumScreenController {
+    public void OnMenuBtnClick(ActionEvent actionEvent) {
+        try {
+            Stage stage = new Stage();
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main_screen.fxml"));
+            Scene mainScene = new Scene(fxmlLoader.load(), 500, 720);
+            stage.setScene(mainScene);
+            stage.show();
+            // Close the current login screen
+            currentStage.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- demo/src/main/java/com/example/demo/MainScreenController.java
+++ demo/src/main/java/com/example/demo/MainScreenController.java
@@ -1,4 +1,43 @@
 package com.example.demo;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static com.example.demo.DocDocGoApplication.currentStage;
+
 public class MainScreenController {
+
+    public void OnReportsBtnClick(ActionEvent actionEvent) {
+        try {
+            Stage stage = new Stage();
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("reports_screen.fxml"));
+            Scene mainScene = new Scene(fxmlLoader.load(), 500, 720);
+            stage.setScene(mainScene);
+            stage.show();
+            // Close the current login screen
+            currentStage.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void OnEmergencyNumberBtnClick(ActionEvent actionEvent) {
+        try {
+            Stage stage = new Stage();
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("emergencyNum_screen.fxml"));
+            Scene mainScene = new Scene(fxmlLoader.load(), 500, 720);
+            stage.setScene(mainScene);
+            stage.show();
+            // Close the current login screen
+            currentStage.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
